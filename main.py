43ac3806dc12a09c4eedbfbdf4a857909dbe25fc
René Delion <rene.leon.delion@gmail.com>
2023-11-06 00:55:51 +0100

    Add Turtle Generator And Move To Start

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from turtle import Turtle, Screen
 
-tur = Turtle()
 screen = Screen()
-
 screen.setup(width=500, height=500)
 
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+turtles = []
+
+for i in COLORS:
+    tur = Turtle("turtle")
+    tur.color(i)
+    tur.penup()
+    turtles.append(tur)
+    tur.goto(x=-230, y=-180 + (len(turtles) * 60))
+
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 
 screen.exitonclick()
